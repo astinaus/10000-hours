b332c0b4900719670d1cb28b1de3e14dd7b4a222
astinaus <slaayers0@gmail.com>
2023-05-11 10:12:36 +0900

    section p태그 -> h2태그로 변경

--- src/Components/InputDisplay/InputDisplay.jsx
+++ src/Components/InputDisplay/InputDisplay.jsx
@@ -35,9 +35,9 @@ export default function InputDisplay({ setInputResult, handleResultDisplay }) {
 
   return (
     <section className="input-container">
-      <q className="quote-title">
+      <h2 className="quote-title">
         “연습은 어제의 당신보다 당신을 더 낫게 만든다.”
-      </q>
+      </h2>
       <q className="quote-desc">
         <strong>1만 시간의 법칙</strong>은<br />
         어떤 분야의 전문가가 되기 위해서는
